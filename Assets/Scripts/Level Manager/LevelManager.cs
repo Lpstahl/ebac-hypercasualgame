4a244c4576c8b3f0dfa959e8ce068a6bdf30a91a
Lpstahl <lpstahl.dev@gmail.com>
2022-11-17 15:45:37 -0300

    add level pieces & PFB_Pieces

--- Assets/Scripts/Level Manager/LevelManager.cs
+++ Assets/Scripts/Level Manager/LevelManager.cs
@@ -6,13 +6,25 @@ public class LevelManager : MonoBehaviour
 {
     public Transform container;
     public List<GameObject> levels;
+    
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+    public int pieceNumberStart = 3;
+    public int pieceNumber = 5;
+    public int pieceNumberEnd = 1;
+    public float timeBetweenPIeces = .3f;
 
     [SerializeField] private int _index;
     private GameObject _currentLevel;
 
+    private List<LevelPieceBase> _spawnedPieces;
+
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -37,7 +49,52 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+    #region
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
 
+        for (int i = 0; i < pieceNumberStart; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+        }        
+        
+        for (int i = 0; i < pieceNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+        }
+
+        for (int i = 0; i < pieceNumberEnd; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+        }
+    }
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < pieceNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+            yield return new WaitForSeconds(timeBetweenPIeces);
+        }
+    }
+    #endregion
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
